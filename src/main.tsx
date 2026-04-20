a49f89dc01f4770ff7e935fb1efebeabf9594dd9
seongbokim <129941781+seongbokim@users.noreply.github.com>
2026-04-20 17:29:03 +0900

    stricmode 잠깐 임의 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
@@ -7,9 +6,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './apis/queryClient.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
 );
