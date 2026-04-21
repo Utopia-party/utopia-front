4d6ee24c1f2fa019c113f1b438e68c9307fc888f
zerose <zerose219@gmail.com>
2026-04-21 14:29:57 +0900

    feat: 결제내역 db연동

--- src/routes.tsx
+++ src/routes.tsx
@@ -29,7 +29,6 @@ import Party from './pages/Party';
 // 마이페이지 관련
 import Profile from './pages/mypage/Profile';
 import MyParty from './pages/mypage/MyParty';
-import MyHistory from './pages/mypage/MyHistory';
 import MyReport from './pages/mypage/MyReport';
 import MyPayment from './pages/mypage/MyPayment';
 import MyTrustHistory from './pages/mypage/MyTrustHistory';
@@ -148,15 +147,11 @@ const router = createBrowserRouter([
             element: <MyParty />,
           },
           {
-            path: 'history',
-            element: <MyHistory />,
-          },
-          {
             path: 'my_trust_history',
             element: <MyTrustHistory />,
           },
           {
-            path: '/mypage/report',
+            path: 'report',
             element: <MyReport />,
           },
           {
